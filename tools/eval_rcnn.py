82be53db607b5b24a292c564b5da2815537e40cd
Shashwat Nigam <mehershashwat@gmail.com>
2021-01-03 01:12:46 +0530

    dgrsd

--- tools/eval_rcnn.py
+++ tools/eval_rcnn.py
@@ -673,7 +673,7 @@ def eval_one_epoch_joint(model, dataloader, epoch_id, result_dir, logger):
 
     if cfg.TEST.SPLIT != 'test':
         logger.info('Averate Precision:')
-        name_to_class = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
+        name_to_class = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2, 'Box': 3}
         ap_result_str, ap_dict = kitti_evaluate(dataset.label_dir, final_output_dir, label_split_file=split_file,
                                                 current_class=name_to_class[cfg.CLASSES])
         logger.info(ap_result_str)
